16ab85f53a3e710f2396d47979c32327a30f186b
leandro <arcanjo.lm@gmail.com>
2023-03-27 10:32:58 -0300

    melhorando a relação da base de dados

--- src/main/java/com/leandro/bookstore/repositories/CategoriaRepository.java
+++ src/main/java/com/leandro/bookstore/repositories/CategoriaRepository.java
@@ -1,11 +1,10 @@
 package com.leandro.bookstore.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.leandro.bookstore.domain.Categoria;
 
-@Repository
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 	
 	
