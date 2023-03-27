9fa91fd434cda3cd9ec48c5192262d700e4a0ab4
leandro <arcanjo.lm@gmail.com>
2023-03-27 19:35:42 -0300

    exceção personalizada para objeto não encontrado

--- src/main/java/com/leandro/bookstore/service/CategoriaService.java
+++ src/main/java/com/leandro/bookstore/service/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.leandro.bookstore.domain.Categoria;
 import com.leandro.bookstore.repositories.CategoriaRepository;
+import com.leandro.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-     
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
+
 	}
 }
